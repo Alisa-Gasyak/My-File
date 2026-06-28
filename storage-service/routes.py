ac59d848076dab639086b1b9590229114a816c4b
Alisa-Gasyak <158738744+Alisa-Gasyak@users.noreply.github.com>
2026-06-29 02:06:09 +0300

    Add files via upload

---
+++ storage-service/routes.py
@@ -0,0 +1,143 @@
+from fastapi import APIRouter, Request, UploadFile, Depends
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
+
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+import os
+import shutil
+import uuid
+
+from .config import Config
+from .db import PklFileEntry, get_session
+from .db.models import ModelStatus
+from .service import PklFileService
+
+storage_router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+database_service = PklFileService()
+
+@storage_router.get("/", response_class=HTMLResponse)
+async def root(
+    request: Request
+):
+    return {"message": "Storage service. Probably would show some data here later."}
+
+
+@storage_router.get("/info/{uid}", response_class=JSONResponse)
+async def get(
+    uid: str,
+    session: AsyncSession = Depends(get_session)
+):
+    try:
+        file_entry: PklFileEntry = await database_service.get_pkl_file_entry(
+            session=session,
+            uid=uid
+        )
+
+        if file_entry:
+            json_data = file_entry.model_dump_json()
+            return JSONResponse(status_code=200, content=json_data)
+        else:
+            raise HTTPException(status_code=404, detail="PklFileEntry with specified uid does not exist.")
+
+    except FileExistsError as e:
+        raise HTTPException(status_code=404, detail="Cannot find file with specified uid.")
+    except PermissionError:
+        raise HTTPException(status_code=409, detail="Cannot read file. PermissionError.")
+    except OSError:
+        raise HTTPException(status_code=500, detail="Unexpected error occured.")
+
+
+@storage_router.get("/get/{uid}", response_class=FileResponse)
+async def get(
+    uid: str,
+    session: AsyncSession = Depends(get_session)
+):
+    try:
+        file_entry: PklFileEntry = await database_service.get_pkl_file_entry(
+            session=session,
+            uid=uid
+        )
+
+        if file_entry:
+            return FileResponse(path=file_entry.filepath)
+        else:
+            raise HTTPException(status_code=404, detail="PklFileEntry with specified uid does not exist.")
+
+    except FileExistsError as e:
+        raise HTTPException(status_code=404, detail="Cannot find file with specified uid.")
+    except PermissionError:
+        raise HTTPException(status_code=409, detail="Cannot read file. PermissionError.")
+    except OSError as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error occured. {e}")
+
+
+@storage_router.get("/info-all")
+async def get_all(
+    session: AsyncSession = Depends(get_session)
+):
+    try:
+        files: PklFileEntry = await database_service.get_all_files(
+            session=session,
+        )
+
+        if files:
+            return files
+
+    except FileExistsError as e:
+        raise HTTPException(status_code=404, detail="Cannot find file with specified uid.")
+    except PermissionError:
+        raise HTTPException(status_code=409, detail="Cannot read file. PermissionError.")
+    except OSError as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error occured. {e}")
+
+
+@storage_router.post("/store", response_class=JSONResponse)
+async def store(
+    pkl_file: UploadFile,
+    session: AsyncSession = Depends(get_session)
+):
+    try:
+        uid = str(uuid.uuid4())
+        name = pkl_file.filename
+        file_path = os.path.join(Config.UPLOAD_DIR, uid)
+        status = ModelStatus.LOADED
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(pkl_file.file, buffer)
+
+        if not os.path.exists(file_path):
+            raise HTTPException(status_code=404, detail="File not found. Failed to create .pkl file?")
+        
+        await database_service.create_pkl_file_entry(
+            session=session,
+            uid=uid,
+            file_path=file_path,
+            status=status,
+            name=name
+        )
+        return JSONResponse(status_code=200, content={"message": "File saved", "uid": uid})
+    
+    except FileExistsError:
+        raise HTTPException(status_code=409, detail="File with set uid already exists.")
+    except PermissionError:
+        raise HTTPException(status_code=409, detail="Cannot create file. PermissionError.")
+    except OSError as e:
+        raise e
+        raise HTTPException(status_code=500, detail=f"Unexpected error occured. {e}")
+
+
+@storage_router.delete("/delete/{uid}", response_class=HTMLResponse)
+async def delete(
+    uid: str,
+    session: AsyncSession = Depends(get_session)
+):
+    file: PklFileEntry = await database_service.get_pkl_file_entry(session=session, uid=uid)
+    await database_service.delete_pkl_file_entry(session=session, uid=uid)
+    os.remove(file.filepath)
+    
+    return JSONResponse(status_code=200, content={"message": "Deleted .pkl file", "uid": uid})
---
+++ trainer-service/routes.py
@@ -0,0 +1,129 @@
+from fastapi import APIRouter, Request, UploadFile, Depends
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
+from fastapi.exceptions import HTTPException
+from starlette.datastructures import Headers
+
+from .ml_pipeline.pipeline_manager import (
+    PredictionPipeline,
+    TrainingPipeline,
+)
+from .schemas import PredictSchema
+
+from typing import List
+from pydantic import BaseModel
+
+import io
+import csv
+import asyncio
+
+from .config import Config
+from .services import storage_service
+
+trainer_router = APIRouter()
+
+@trainer_router.post("/train")
+async def train(
+    csv_file: UploadFile
+):
+    try:
+        # Scary? I dunno
+        csv_bytes = await csv_file.read()
+
+        trainer = TrainingPipeline(csv_bytes)
+        trainer.run_pipeline()
+
+        model_name = csv_file.filename
+        model_bytes = await asyncio.to_thread(trainer.get_model_bytes)
+        metrics = trainer.get_metrics()
+
+        file_stream = io.BytesIO(model_bytes)
+        upload_file = UploadFile(
+            file=file_stream, 
+            filename=model_name,
+            headers=Headers({"content-type": "text/plain"}),
+            size=len(model_bytes)
+        )
+
+        # saving .pkl model on storage server
+        response = await storage_service.save_pkl_file(file=upload_file)
+        response_data = response.json()
+        print(response_data)
+        json_data = {
+            "message": "Model successfully trained",
+            "uid": response_data["uid"]
+        }
+        return JSONResponse(content=json_data, status_code=200)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Could not train model")
+
+# FOR PREDICTOR
+
+predictor_router = APIRouter()
+
+csv_fieldnames = [
+    "person_age",
+    "person_gender",
+    "person_education",
+    "person_income",
+    "person_emp_exp",
+    "person_home_ownership",
+    "loan_amnt",
+    "loan_intent",
+    "loan_int_rate",
+    "loan_percent_income",
+    "cb_person_cred_hist_length",
+    "credit_score",
+    "previous_loan_defaults_on_file",
+    "loan_status"
+    ]
+
+@predictor_router.post("/predict/{uid}")
+async def predict(
+    uid: str,
+    PredictSchema: PredictSchema
+):
+    try:
+        data = PredictSchema.data
+        buffer = io.StringIO()
+        writer = csv.DictWriter(buffer, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+        print(buffer.getvalue().encode('utf-8'))
+        csv_bytes = buffer.getvalue().encode('utf-8')
+
+        model =  await storage_service.get_pkl_file(uid=uid)
+        model_bytes = model.content
+
+        prediction_pipeline = PredictionPipeline(csv_bytes, model_bytes)
+        prediction_pipeline.run_pipeline()
+
+        prediction = prediction_pipeline.get_prediction()
+        response = {
+            "message": "Successeful prediction",
+            "predicion": prediction.tolist()
+        }
+        return JSONResponse(content=response, status_code=200)
+    except:
+        raise HTTPException(status_code=500, detail="Unecpected error")
+
+@predictor_router.post("/predict_csv/{uid}")
+async def predict_csv(
+    uid: str,
+    csv_file: UploadFile
+):
+    try:
+        csv_bytes = await csv_file.read()
+        model =  await storage_service.get_pkl_file(uid=uid)
+        model_bytes = model.content
+
+        prediction_pipeline = PredictionPipeline(csv_bytes, model_bytes)
+        prediction_pipeline.run_pipeline()
+
+        prediction = prediction_pipeline.get_prediction()
+        response = {
+            "message": "Successeful prediction",
+            "predicion": prediction.tolist()
+        }
+        return JSONResponse(content=response, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Unecpected error")
